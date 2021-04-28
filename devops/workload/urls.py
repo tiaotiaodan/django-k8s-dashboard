bfd6b8df66419a41e0caf5fa6943acbf92984708
shichao <shichao@scajy.cn>
2021-04-28 20:50:54 +0800

    提交学习修改

--- devops/workload/urls.py
+++ devops/workload/urls.py
@@ -5,7 +5,13 @@ from workload import views
 
 urlpatterns = [
     re_path('^deployments/$',views.deployments, name="deployments"),
+    re_path('^daemonsets/$',views.daemonsets, name="daemonsets"),
+    re_path('^statefulsets/$',views.statefulsets, name="statefulsets"),
+    re_path('^pods/$',views.pods, name="pods"),
     re_path('^deployments_api/$',views.deployments_api, name="deployments_api"),
+    re_path('^daemonsets_api/$',views.daemonsets_api, name="daemonsets_api"),
+    re_path('^statefulsets_api/$', views.statefulsets_api, name="statefulsets_api"),
+    re_path('^pods_api/$', views.pods_api, name="pods_api"),
 
 ]
 
--- devops/workload/views.py
+++ devops/workload/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse, QueryDict
 from kubernetes import client, config
-import os,hashlib,random
+import os, hashlib, random
 from devops import k8s_tools  # 导入k8s登陆封装
 
 
-# Create your views here.
+# deployments 页面展示
 def deployments(request):
     return render(request, "workload/deployments.html")
 
+
+# daemonset 页面展示
+def daemonsets(request):
+    return render(request, "workload/daemonsets.html")
+
+
+# statefulset 页面展示
+def statefulsets(request):
+    return render(request, "workload/statefulsets.html")
+
+# pods 页面展示
+def pods(request):
+    return render(request, "workload/pods.html")
 
 # deployments_api接口
 def deployments_api(request):
@@ -49,9 +62,13 @@ def deployments_api(request):
                 dp = {"name": name, "namespace": namespace, "replicas": replicas,
                       "available_replicas": available_replicas, "labels": labels, "selector": selector,
                       "images": images, "create_time": create_time}
-                # 根据搜索值返回数据
+
+                # 根据前端返回的搜索key进行判断，查询关键字返回数据
+                search_key = request.GET.get('searchkey', None)
                 if search_key:
-                    if search_key in name:
+                    if search_key == name:
+                        data.append(dp)
+                    elif search_key in name:
                         data.append(dp)
                 else:
                     data.append(dp)
@@ -78,9 +95,9 @@ def deployments_api(request):
     elif request.method == "DELETE":
         request_data = QueryDict(request.body)
         name = request_data.get("name")
-        namespace = request_data.get("namespace")
+        namespace = request_data.get("namespace")  # 获取命名空间
         try:
-            apps_api.delete_namespaced_deployment(namespace=namespace, name=name)
+            apps_api.delete_namespaced_deployment(namespace=namespace, name=name)  # 删除命名空间下的deployment服务
             code = 0
             msg = "删除成功."
         except Exception as e:
@@ -92,3 +109,256 @@ def deployments_api(request):
                 msg = "删除失败！"
         res = {'code': code, 'msg': msg}
         return JsonResponse(res)
+
+
+# daemonsets_api 接口
+def daemonsets_api(request):
+    code = 0
+    msg = ""
+    auth_type = request.session.get("auth_type")
+    token = request.session.get("token")
+    k8s_tools.load_auth_config(auth_type, token)
+    apps_api = client.AppsV1Api()
+    if request.method == "GET":
+        namespace = request.GET.get("namespace")
+        data = []
+        try:
+            for ds in apps_api.list_namespaced_daemon_set(namespace=namespace).items:
+                name = ds.metadata.name
+                namespace = ds.metadata.namespace
+                desired_number = ds.status.desired_number_scheduled
+                available_number = ds.status.number_available
+                labels = ds.metadata.labels
+                selector = ds.spec.selector.match_labels
+                containers = {}
+                for c in ds.spec.template.spec.containers:
+                    containers[c.name] = c.image
+                create_time = ds.metadata.creation_timestamp
+
+                ds = {"name": name, "namespace": namespace, "labels": labels, "desired_number": desired_number,
+                      "available_number": available_number,
+                      "selector": selector, "containers": containers, "create_time": create_time}
+
+                # 根据前端返回的搜索key进行判断，查询关键字返回数据
+                search_key = request.GET.get('searchkey', None)
+                if search_key:
+                    if search_key == name:
+                        data.append(ds)
+                    elif search_key in name:
+                        data.append(ds)
+                else:
+                    data.append(ds)
+                code = 0
+                msg = "获取数据成功"
+        except Exception as e:
+            code = 1
+            status = getattr(e, "status")
+            if status == 403:
+                msg = "没有访问权限"
+            else:
+                msg = "获取数据失败"
+        count = len(data)
+
+        page = int(request.GET.get('page', 1))
+        limit = int(request.GET.get('limit'))
+        start = (page - 1) * limit
+        end = page * limit
+        data = data[start:end]
+
+        res = {'code': code, 'msg': msg, 'count': count, 'data': data}
+        return JsonResponse(res)
+
+    elif request.method == "DELETE":
+        request_data = QueryDict(request.body)
+        name = request_data.get("name")
+        namespace = request_data.get("namespace")  # 获取命名空间
+        try:
+            apps_api.delete_namespaced_daemon_set(namespace=namespace, name=name)  # 删除命名空间下的deployment服务
+            code = 0
+            msg = "删除成功."
+        except Exception as e:
+            code = 1
+            status = getattr(e, "status")
+            if status == 403:
+                msg = "没有删除权限"
+            else:
+                msg = "删除失败！"
+        res = {'code': code, 'msg': msg}
+        return JsonResponse(res)
+
+
+# statefulsets_api 接口
+def statefulsets_api(request):
+    code = 0
+    msg = ""
+    auth_type = request.session.get("auth_type")
+    token = request.session.get("token")
+    k8s_tools.load_auth_config(auth_type, token)
+    apps_api = client.AppsV1Api()
+    if request.method == "GET":
+        namespace = request.GET.get("namespace")
+        data = []
+        try:
+            for sts in apps_api.list_namespaced_stateful_set(namespace).items:
+                name = sts.metadata.name
+                namespace = sts.metadata.namespace
+                labels = sts.metadata.labels
+                selector = sts.spec.selector.match_labels
+                replicas = sts.spec.replicas
+                ready_replicas = ("0" if sts.status.ready_replicas is None else sts.status.ready_replicas)
+                # current_replicas = sts.status.current_replicas
+                service_name = sts.spec.service_name
+                containers = {}
+                for c in sts.spec.template.spec.containers:
+                    containers[c.name] = c.image
+                create_time = sts.metadata.creation_timestamp
+
+                sts = {"name": name, "namespace": namespace, "labels": labels, "replicas": replicas,
+                       "ready_replicas": ready_replicas, "service_name": service_name,
+                       "selector": selector, "containers": containers, "create_time": create_time}
+                # 根据前端返回的搜索key进行判断，查询关键字返回数据
+                search_key = request.GET.get('searchkey', None)
+                if search_key:
+                    if search_key == name:
+                        data.append(sts)
+                    elif search_key in name:
+                        data.append(sts)
+                else:
+                    data.append(sts)
+                code = 0
+                msg = "获取数据成功"
+        except Exception as e:
+            code = 1
+            status = getattr(e, "status")
+            if status == 403:
+                msg = "没有访问权限"
+            else:
+                msg = "获取数据失败"
+        count = len(data)
+
+        page = int(request.GET.get('page', 1))
+        limit = int(request.GET.get('limit'))
+        start = (page - 1) * limit
+        end = page * limit
+        data = data[start:end]
+
+        res = {'code': code, 'msg': msg, 'count': count, 'data': data}
+        return JsonResponse(res)
+
+    elif request.method == "DELETE":
+        request_data = QueryDict(request.body)
+        name = request_data.get("name")
+        namespace = request_data.get("namespace")  # 获取命名空间
+        try:
+            apps_api.delete_namespaced_daemon_set(namespace=namespace, name=name)  # 删除命名空间下的deployment服务
+            code = 0
+            msg = "删除成功."
+        except Exception as e:
+            code = 1
+            status = getattr(e, "status")
+            if status == 403:
+                msg = "没有删除权限"
+            else:
+                msg = "删除失败！"
+        res = {'code': code, 'msg': msg}
+        return JsonResponse(res)
+
+
+# pods_api 接口
+def pods_api(request):
+    code = 0
+    msg = ""
+    auth_type = request.session.get("auth_type")
+    token = request.session.get("token")
+    k8s_tools.load_auth_config(auth_type, token)
+    core_api = client.CoreV1Api()
+    if request.method == "GET":
+        namespace = request.GET.get("namespace")
+        data = []
+        try:
+            for po in core_api.list_namespaced_pod(namespace).items:
+                name = po.metadata.name
+                namespace = po.metadata.namespace
+                labels = po.metadata.labels
+                pod_ip = po.status.pod_ip
+
+                containers = []  # [{},{},{}]
+                status = "None"
+                # 只为None说明Pod没有创建（不能调度或者正在下载镜像）
+                if po.status.container_statuses is None:
+                    status = po.status.conditions[-1].reason
+                else:
+                    for c in po.status.container_statuses:
+                        c_name = c.name
+                        c_image = c.image
+
+                        # 获取重启次数
+                        restart_count = c.restart_count
+
+                        # 获取容器状态
+                        c_status = "None"
+                        if c.ready is True:
+                            c_status = "Running"
+                        elif c.ready is False:
+                            if c.state.waiting is not None:
+                                c_status = c.state.waiting.reason
+                            elif c.state.terminated is not None:
+                                c_status = c.state.terminated.reason
+                            elif c.state.last_state.terminated is not None:
+                                c_status = c.last_state.terminated.reason
+
+                        c = {'c_name': c_name, 'c_image': c_image, 'restart_count': restart_count, 'c_status': c_status}
+                        containers.append(c)
+
+                create_time = po.metadata.creation_timestamp
+
+                po = {"name": name, "namespace": namespace, "pod_ip": pod_ip,
+                      "labels": labels, "containers": containers, "status": status,
+                      "create_time": create_time}
+                # 根据前端返回的搜索key进行判断，查询关键字返回数据
+                search_key = request.GET.get('searchkey', None)
+                if search_key:
+                    if search_key == name:
+                        data.append(po)
+                    elif search_key in name:
+                        data.append(po)
+                else:
+                    data.append(po)
+                code = 0
+                msg = "获取数据成功"
+        except Exception as e:
+            code = 1
+            status = getattr(e, "status")
+            if status == 403:
+                msg = "没有访问权限"
+            else:
+                msg = "获取数据失败"
+        count = len(data)
+
+        page = int(request.GET.get('page', 1))
+        limit = int(request.GET.get('limit'))
+        start = (page - 1) * limit
+        end = page * limit
+        data = data[start:end]
+
+        res = {'code': code, 'msg': msg, 'count': count, 'data': data}
+        return JsonResponse(res)
+
+    elif request.method == "DELETE":
+        request_data = QueryDict(request.body)
+        name = request_data.get("name")
+        namespace = request_data.get("namespace")  # 获取命名空间
+        try:
+            core_api.delete_namespaced_pod(namespace=namespace, name=name)  # 删除命名空间下的deployment服务
+            code = 0
+            msg = "删除成功."
+        except Exception as e:
+            code = 1
+            status = getattr(e, "status")
+            if status == 403:
+                msg = "没有删除权限"
+            else:
+                msg = "删除失败！"
+        res = {'code': code, 'msg': msg}
+        return JsonResponse(res)
+
